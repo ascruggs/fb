78df9432e5975d69cd127416d52468ec2db6287d
ascruggs <aascruggs@gmail.com>
2011-04-15 22:43:44 -0500

    refining

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,14 @@ class User < ActiveRecord::Base
   #devise :database_authenticatable, :registerable,
   #       :recoverable, :rememberable, :trackable, :validatable
          
-  devise :omniauthable, :database_authenticatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :token
+  devise :omniauthable
   
    def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token['extra']['user_hash']
     if user = User.find_by_email(data["email"])
       user
     else # Create a user with a stub password. 
-      User.create!(:email => data["email"], :password => Devise.friendly_token[0,20], :token => access_token['credentials']['token']) 
+      User.create!(:email => data["email"], :token => access_token['credentials']['token']) 
     end
   end
 end

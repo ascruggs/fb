724dbcbfb1b4886067f50f4c38414b9211744063
ascruggs <aascruggs@gmail.com>
2011-04-15 21:08:49 -0500

    just fucking around

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'APP_ID', 'APP_SECRET'  # Find these values on the Facebook App page
+  provider :facebook, '219461168071118', '40f66e3a471361da450c10d7c271773f', {:scope => 'publish_stream,offline_access,email'}
 end
